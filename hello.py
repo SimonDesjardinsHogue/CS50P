da6b775cddd3172446c5be94ab467b300989c334
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-22 16:33:39 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,5 @@
+# Ask user for their name and remove whitespace from str (with method strip) and Capitalize user's name (premiere lettre de chaque lettre)
+name = input("What's your name? ").strip().title()
+
+# Say hello to user
+print (f"hello, {name}")
