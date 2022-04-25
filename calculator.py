924b154da5f38c625cca1f6f530b3bb1379ef209
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-25 12:31:58 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,29 @@
-x = float(input("What's x? "))
-y = float("What's y? "))
+def main():
+  x = int(input("What's x? "))
+  print("x squared is", square(x))
+  
+  
+def square(n):
+    return pow(n, 2)
+  
+  
+main()
 
-z = round (x + y)
-
-print(f"{z:,}")
 
 
-
-x = float(input("What's x? "))
-y = float("What's y? "))
-
-z = round (x / y, 2)
-
-print(f"{z:,}")
+  
+# y = float("What's y? "))
+#
+#
+# z = round (x + y)
+#
+# print(f"{z:,}")
+#
+#
+#
+# x = float(input("What's x? "))
+# y = float("What's y? "))
+#
+# z = round (x / y, 2)
+#
+# print(f"{z:,}")
