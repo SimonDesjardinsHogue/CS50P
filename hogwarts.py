b5d559809fe91574b3c797d6171747ded5e97504
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-26 16:11:37 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -15,4 +15,18 @@ students = {
   "Draco": "Slytherin",
 }
 
-print(students["Hermione"])
+for student in students:
+  print(student, students [student], sep=", ")
+
+  
+  # avec list de dict
+  
+  students = [
+    {"name":"Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name":"Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name":"Ron", "house": "Gryffindor", "patronus": "Jack Russell terrier"},
+    {"name":"Draco", "house": "Slytherin", "patronus": None}
+  ]
+  
+  for student in students:
+    print(student["name"], student["house], student["patronus"], sep". ")
