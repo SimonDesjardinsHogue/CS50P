82f50967433d8a9d4a5fdb39b42d742c2a01dc8c
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-26 14:44:55 -0400

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,4 +1,9 @@
-i = 3
-while i !=0:
-    print("meow")
-    i  = i - 1
+# en utilisant une loop
+# i = 0
+# while i < 3:
+#     print("meow")
+#     i  += 1
+
+# en utilisant une list
+for _ in range(3):
+        print("meow")
