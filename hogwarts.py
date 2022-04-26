b6b6a12c538d1096a287d8ef09c8c529687dedcb
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-26 15:51:53 -0400

    Update hogwarts.py

--- hogwarts.py
+++ hogwarts.py
@@ -1,5 +1,18 @@
-students = ["Hermione", "Harry", "Ron"]
+#students = ["Hermione", "Harry", "Ron"]
+#houses = ["Gryffindor", "Gryffindor", "Grffindor", "Slytherin"]
+#for student in students:
+#  prints(students)
+#  
+# for i in range(len(students)):
+#   print(i + 1, students[1])
+#
+#
+# using dict
+students = {
+  "Hermione": "Gryffindor,
+  "Harry": "Gryffindor",
+  "Ron": "Gryffindor",
+  "Draco": "Slytherin",
+}
 
-for student in students:
-  prints(students)
-  
+print(students["Hermione"])
