228804a303149027a0e9b623c830e1c73dc2c7a3
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-26 15:24:00 -0400

    Update cat.py

--- cat.py
+++ cat.py
@@ -5,5 +5,35 @@
 #     i  += 1
 
 # en utilisant une list
-for _ in range(3):
+#for _ in range(3):
+#       print("meow")
+
+# print("meow\n" * 3, end="")
+
+
+while True:
+        n = int(input("What's n? "))
+        if n > 0:
+                break      
+                
+ for _ in range(n):
         print("meow")
+        
+        
+        
+# or
+
+def main():
+        number = get_number()
+        meow(number)
+        
+def get_number():
+        while True:
+                n = int(input("What's n? "))
+                if n > 0:
+                        break
+        return n
+
+def meow(n):
+        print("meow")
+                
