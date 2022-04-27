26bb6f644f1560eece2f894f72eed546a484d419
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-27 13:01:22 -0400

    Create tip.py

---
+++ tip.py
@@ -0,0 +1,20 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+    # TODO
+    d = float(dollars)
+    dollars = d.lstrip("$")
+
+
+def percent_to_float(p):
+    # TODO
+    p = float(percent)
+    percent = p.rstrip("%")
+
+
+main()
