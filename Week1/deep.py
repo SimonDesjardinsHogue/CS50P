a7c1d23407021b8b27483fa4bcaca955b49c6f19
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-27 19:11:12 -0400

    Update deep.py

--- Week1/deep.py
+++ Week1/deep.py
@@ -1,15 +1,11 @@
 def main():
-    x = input("What is the Answer to the Great Question of Life, the Universe and Everything")
-    condition(x)
-
-def condition(x):
-    if x == 42:
-        print("Yes")
-    elif x == forty-two:
-        print("Yes")
-    elif x == "forty two":
+    x = input("What is the Answer to the Great Question of Life, the Universe and Everything\n")
+    if reponse(x):
         print("Yes")
     else:
         print("No")
+
+def reponse(x):
+    return x == "42" or x == "forty-two" or x == "forty two"
 
 main()
