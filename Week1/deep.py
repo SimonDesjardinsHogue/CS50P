3fe170cdfaf800cdfd72a6ca4547fb5d567a65de
SimonDesjardinsHogue <25335065+SimonDesjardinsHogue@users.noreply.github.com>
2022-04-27 19:04:05 -0400

    Create deep.py

---
+++ Week1/deep.py
@@ -0,0 +1,15 @@
+def main():
+    x = input("What is the Answer to the Great Question of Life, the Universe and Everything")
+    condition(x)
+
+def condition(x):
+    if x == 42:
+        print("Yes")
+    elif x == forty-two:
+        print("Yes")
+    elif x == "forty two":
+        print("Yes")
+    else:
+        print("No")
+
+main()
